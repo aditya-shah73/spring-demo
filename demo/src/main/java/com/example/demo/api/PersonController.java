54bd545061db8fea9ac1cb07e8f8069a67e01e1b
Aditya Shah <aditya7395@gmail.com>
2020-08-02 19:19:23 -0700

    Spring framework setup and creating a few Rest APIs

---
+++ demo/src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,50 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    //The RequestBody annotation will basically accept a person as a request body
+    public void addPerson(@RequestBody Person person){
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+    //path is specified so that the id can be passed in the url like api/v1/person/259c3a4f-d0be-47b3-b844-0194248f157c
+    @GetMapping(path = "{id}")
+    //The path variable annotation will simply grab the id and turn it into a UUID
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(@PathVariable("id") UUID id) {
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePerson(@PathVariable("id") UUID id, @NonNull @RequestBody Person personToUpdate){
+        personService.updatePerson(id, personToUpdate);
+    }
+}
